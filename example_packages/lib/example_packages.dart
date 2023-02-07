129415d3c82057ab624d19dca28930b76ac2bda3
Fabrizio Cacicia <mail@fabriziocacicia.com>
2023-02-07 11:59:56 +0100

    feat: update stricter type checks options to the new format (#10)

---
+++ example_packages/lib/example_packages.dart
@@ -0,0 +1,16 @@
+// public_member_api_docs
+class Bad {
+  void init() { }
+}
+
+/// Base of all things.
+abstract class Base {
+  /// Initialize the base.
+  void init();
+}
+
+/// A sub base.
+class Sub extends Base {
+  @override
+  void init() {  }
+}
